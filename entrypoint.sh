7d4852ffd8ed65d3e26359fcf1ae4656cbee6980
andrewagrahamhodges <55956144+andrewagrahamhodges@users.noreply.github.com>
2020-05-06 16:20:10 +0200

    Start ssh server

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,6 +6,9 @@ dbus-daemon --system
 # create the sock dir properly
 /bin/sh /usr/share/xrdp/socksetup
 
+# start openssh-server
+/etc/init.d/ssh start &
+
 # run xrdp and xrdp-sesman in the foreground so the logs show in docker
 xrdp-sesman -ns &
 xrdp -ns
